f6afb1b77e4c62c7c1cdac6ed25a940f11d57ec1
Bishnu <bishnuthapako@gmail.com>
2022-07-19 17:26:39 -0400

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,53 @@
+require "pry"
 
+class Book
+
+    def initialize(title)
+        @title = title
+    end
+
+    def genre
+        @genre
+    end
+
+    def genre=genre
+        @genre=genre
+    end
+
+    def title
+        @title
+    end
+    def title=title
+        @title=title
+    end
+
+    def author
+        @author
+    end
+    def author=author
+        @author=author
+    end
+    def page_count
+        @page_count
+    end
+    def page_count=page_count
+        @page_count=page_count
+    end
+
+
+    # def properties(author, page, genre)
+    #     @author, @page_count, @genre = author, page_count, genre
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        
+    end
+
+
+    
+end
+
+# book = Book.new
+# binding.pry
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+
+require "pry"
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition, :shoe
+
+    def initialize(brand)
+        @brand= brand
+    end
+
+    def properties(color, size, material, condition)
+        @color, @size, @material, @condition = color, size, material, condition
+    end
+
+    def cobble
+        self.condition="new"
+  
+        puts "Your shoe is as good as new!"
+    end
+       
+end
+
